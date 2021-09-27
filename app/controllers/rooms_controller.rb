82839e78c71c91b96b4c76017cb39b1ac6477a0c
ahmed abdelgawad <ahmed.abdelgawad.farag@gmail.com>
2021-09-27 14:11:47 +0200

    add rooms routes

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,17 @@
+class RoomsController < ApplicationController
+
+  def index
+    rooms = User.find(params[:user_id]).rooms
+
+    farr = []
+
+    for i in rooms
+      farr.push({ "room_id": i.id, "users": i.users })
+    end
+
+
+    render json: farr
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-
   mount ActionCable.server => '/cable'
 
   resources :users, only: [:index, :create]
   resources :auth, only: [:create]
 
   resources :users do
+    resources :rooms, only: [:index]
+  end
+
+  resources :users do
     resources :friendships do
-      resources :messages, only: [:create,:index]
+      resources :messages, only: [:create, :index]
     end
   end
 
   resources :users do
-    resources :friendships,only: [:index]
+    resources :friendships, only: [:index]
   end
 
 end
